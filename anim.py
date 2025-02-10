1fa1729efd96c0499f44d0d78930db4c1f4d3566
Porog <anatolijmordvin31@gmail.com>
2025-02-10 07:27:03 +0300

    Почти итоговый коммит

--- anim.py
+++ anim.py
@@ -11,21 +11,22 @@ cur = con.cursor()
 result = cur.execute("""SELECT name FROM users""").fetchall()
 id = 0
 sz = 0
+
 for i in result:
     if i[0] == name:
         print('Вы уже зарегистрированы')
-        id = sz
         break
     sz += 1
 else:
     cur.execute(f"""INSERT INTO users (name, klass, level) VALUES (?, ?, ?)""", (name, random.randint(1, 3), random.randint(1, 3)))
     con.commit()
     print('Вы успешно зарегистрированы')
-    id = len(result)
-print('Товары в магазине можно купить, выбрав их и нажав кнопку мыши')
+
 result = cur.execute("""SELECT * FROM users """).fetchall()
 FPS = 50
 pygame.init()
+pygame.mixer.music.load('fon.mp3')
+pygame.mixer.music.play()
 pname = ['woin.png', 'mag.png', 'wor.png']
 ppname = ['woinb.png', 'mb.png', 'worb.png']
 sname = ['woins.png', 'mags.png', 'wors.png']
@@ -75,6 +76,7 @@ class MagSprite(pygame.sprite.Sprite):
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
+
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
@@ -85,6 +87,7 @@ class MagSprite(pygame.sprite.Sprite):
         if self.f:
             keys = pygame.key.get_pressed()
             move = None
+
             if keys[pygame.K_UP]:
                 move = 'up'
                 self.rect.y -= 5
@@ -97,12 +100,15 @@ class MagSprite(pygame.sprite.Sprite):
             elif keys[pygame.K_RIGHT]:
                 move = 'right'
                 self.rect.x += 5
+
             if move:
                 self.sz += 1
+
                 if self.sz > self.counter:
                     self.sz = 0
                     self.cur_frame = ((self.cur_frame + 1) % self.columns)
                     self.image = pygame.transform.scale(self.frames[self.cur_frame + self.d[move] * self.columns], (147 * 0.8, 133 * 0.8))
+
                     if move == 'right':
                         self.image = pygame.transform.flip(self.image, True, False)
 
@@ -130,6 +136,7 @@ class WoinWorSprite(pygame.sprite.Sprite):
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
+
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
@@ -140,6 +147,7 @@ class WoinWorSprite(pygame.sprite.Sprite):
         if self.f:
             keys = pygame.key.get_pressed()
             move = None
+
             if keys[pygame.K_UP]:
                 move = 'up'
                 self.rect.y -= 5
@@ -227,6 +235,7 @@ def start_screen():
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
+
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
@@ -243,6 +252,7 @@ def start_screen():
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 return
+
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -260,35 +270,45 @@ if __name__ == '__main__':
     Tile()
     Tor = Torg()
     Por = Port()
+
     if tb != 1:
         hero = WoinWorSprite(load_image(polz, colorkey=-1), 4, 1, 400, 400)
     else:
         hero = MagSprite(load_image(polz, colorkey=-1), 5, 3, 400, 400)
+
     sz = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         if pygame.sprite.collide_mask(hero, Tor):
             if up:
                 sp.append(Tow('exit.png', 200, 400))
                 sp.append(Tow('levelup.png', 600, 400))
+
                 f = 1
                 sz += 1
                 hero.mover()
+
                 if sz == 2:
                     sz = 0
                     hero.imover()
+
         if pygame.sprite.collide_mask(hero, Por) and not f:
             break
+
         all_sprites.update()
         screen.fill((0, 0, 0))
         tiles_group.draw(screen)
+
         if f:
             tov_group.draw(screen)
+
             if pygame.sprite.collide_mask(hero, sp[0]) and event.type == pygame.MOUSEBUTTONDOWN:
                 f = 0
+
             elif pygame.sprite.collide_mask(hero, sp[1]) and event.type == pygame.MOUSEBUTTONDOWN:
                 tl = result[id][3] + 1
                 con.commit()
@@ -296,9 +316,11 @@ if __name__ == '__main__':
                 cur.execute(f"UPDATE users SET level = ? WHERE name = ?", (tl, name))
                 f = 0
                 up = False
+
         all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
+
 
 polz = ppname[tb]
 all_sprites = pygame.sprite.Group()
@@ -319,6 +341,7 @@ class MagB(pygame.sprite.Sprite):
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
+
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
@@ -343,6 +366,7 @@ class Wrag1(pygame.sprite.Sprite):
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
+
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
@@ -356,19 +380,19 @@ class Wrag1(pygame.sprite.Sprite):
 
 
 def end_screen():
-    intro_text = ["Вы умерли, ваш конечный уровень равен" + str(tl - 1)]
+    intro_text = "Вы умерли, ваш конечный уровень равен" + '  ' + str(tl - 1)
     fon = pygame.transform.scale(load_image('end.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+
+    string_rendered = font.render(intro_text, 1, pygame.Color('white'))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = text_coord
+    intro_rect.x = 400
+    intro_rect.y = 500
+    print(string_rendered, intro_rect)
+    screen.blit(string_rendered, intro_rect)
 
     while True:
         for event in pygame.event.get():
@@ -380,21 +404,26 @@ def end_screen():
         pygame.display.flip()
         clock.tick(FPS)
 
+
 if __name__ == '__main__':
     pygame.init()
     pl = 0
-    tl = result[id][3]
+    tl = result[id - 1][3]
     f = 1
     f1 = 1
     ws = 0
+
     hph = tl * random.randint(3, 10)
     hpw = tl * random.randint(1, 6)
     nhh = hph
     nhw = hpw
+
     if tb != 0:
         hero = MagB(load_image(polz, colorkey=-1), 4, 1, 300, 400)
+
     else:
         hero = MagB(load_image(polz, colorkey=-1), 8, 1, 300, 400)
+
     wrag = Wrag1(load_image('wrag1.png', colorkey=-1), 8, 1, 500, 400)
     screen = pygame.display.set_mode(size)
     screen_rect = (0, 0, width, height)
@@ -404,6 +433,7 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         all_sprites.update()
         screen.fill((0, 0, 0))
         tiles_group.draw(screen)
@@ -411,20 +441,27 @@ if __name__ == '__main__':
         ser_group.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
+
         if f and f1:
             a = random.randint(1, 5) * tl // 10
             b = random.randint(1, 8) * tl // 10
+
             if a == 0:
                 a = 1
+
             if b == 0:
                 b = 1
+
             hph -= a
             hpw -= b
             khh = (hph - 1) / nhh
             khw = (hpw - 1) / nhw
+
             ser_group = pygame.sprite.Group()
+
             for i in range(int(khh // 0.2) + 1):
                 Ser(i * 25 + 400, 400)
+
             for i in range(int(khw // 0.2) + 1):
                 Ser(i * 25 + 600, 400)
 
@@ -435,10 +472,12 @@ if __name__ == '__main__':
                 ser_group = pygame.sprite.Group()
                 f = 0
                 sz = 0
+
             elif hpw <= 0:
                 pl += 1
                 f1 = 0
                 sz = 0
+
                 if pl >= tl:
                     tl += 1
                     con.commit()
@@ -446,6 +485,7 @@ if __name__ == '__main__':
                     cur.execute(f"UPDATE users SET level = ? WHERE name = ?", (tl, name))
                     pl = 0
                     ws = 0
+
                 all_sprites = pygame.sprite.Group()
                 Win()
                 ws = (ws + 1) % 2
@@ -454,26 +494,31 @@ if __name__ == '__main__':
                 nhh = hph
                 nhw = hpw
                 ser_group = pygame.sprite.Group()
+
                 if ws:
-                    wrag = Wrag1(load_image('wrag1s.png', colorkey=-1), 6, 1, 500, 400)
+                    Wrag1(load_image('wrag1s.png', colorkey=-1), 6, 1, 500, 400)
                 else:
-                    wrag = Wrag1(load_image('wrag2s.png', colorkey=-1), 6, 1, 500, 400)
+                    Wrag1(load_image('wrag2s.png', colorkey=-1), 6, 1, 500, 400)
+
         elif f1:
             sz += 1
             if sz == 5:
                 end_screen()
-                print('Вы умерли, ваш конечный уровень равен', tl - 1)
                 break
+
         else:
             sz += 1
             if sz == 5:
                 all_sprites = pygame.sprite.Group()
+
                 if tb != 0:
                     hero = MagB(load_image(polz, colorkey=-1), 4, 1, 300, 400)
                 else:
                     hero = MagB(load_image(polz, colorkey=-1), 8, 1, 300, 400)
+
                 if ws:
-                    wrag = Wrag1(load_image('wrag2.png', colorkey=-1), 5, 1, 500, 400)
+                    Wrag1(load_image('wrag2.png', colorkey=-1), 5, 1, 500, 400)
                 else:
-                    wrag = Wrag1(load_image('wrag1.png', colorkey=-1), 8, 1, 500, 400)
+                    Wrag1(load_image('wrag1.png', colorkey=-1), 8, 1, 500, 400)
+
                 f1 = 1
